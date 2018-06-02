cad5acd866fdbc513ef120a6147f1558098b6c28
韩伟 <770811054@qq.com>
2018-06-02 13:38:08 +0800

    Revert "aaa"
    This reverts commit 0a877ef92cf6d2beca45a5757782301e85ea71b0.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {BigHomeComponent} from './big-home/big-home.component';
-import {HomeComponent} from './home/home.component';
-import {NewsComponent} from './news/news.component';
-import {AskComponent} from './ask/ask.component';
-import {ExchangeComponent} from './exchange/exchange.component';
-import {VideoComponent} from './video/video.component';
-import {ZhiboComponent} from './zhibo/zhibo.component';
-import {MineComponent} from './mine/mine.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
 
-const routes: Routes = [
-  // 重定向路由
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'big-home', component: BigHomeComponent , children: [
-      {path: '', component: HomeComponent},
-      {path: 'home', component: HomeComponent},
-      {path: 'news', component: NewsComponent},
-      {path: 'ask', component: AskComponent},
-      {path: 'exchange', component: ExchangeComponent},
-      {path: 'video', component: VideoComponent},
-      {path: 'zhibo', component: ZhiboComponent},
-      {path: 'mine', component: MineComponent}
-    ]},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
